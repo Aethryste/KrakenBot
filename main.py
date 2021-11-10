8a53379b0179a3d8cd880b825282e59fa42fa65f
Richard Algra <richard.b.algra@gmail.com>
2021-11-10 21:41:36 +0100

    Added: RSI, Bollinger-bands, (basic)paper-trading. Working on: Kraken specific functions (bal, buy, sell, auth) Note: RSI value is extremely volatile, bug?

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
-# Resources
+# Resources.
 import krakenex
 from pykrakenapi import KrakenAPI
+import config
 import time
 import pandas as pd
+from numpy import floor
 
-# API init
-api = krakenex.API()
-k = KrakenAPI(api)
+# User-settings (config.py).
+C_interval = config.collection_interval
+RSI_sell = config.RSI_sell
+RSI_buy = config.RSI_buy
+TICKER = config.ticker
+BPD = config.buy_price_deviation
 
-# Globals & settings
+# Globals
 running = True
-interval = 60  # int(1) == new data every 1 second.
-RSI_sell = 70  # [RSI] overbought trigger.
-RSI_buy = 30  # {RSI} oversold trigger.
-
+RSI_ready = False
+BB_ready = False
+account_balance = 0
+last_RSI = 50  # RSI initial state, to prevent false triggers at startup.
 price_high = 0
 price_low = 0
 price_close = 0
-last_RSI = 50  # [RSI] init.
+bollinger_upper = 0
+bollinger_lower = 0
+
+# List of (trading)triggers.
+T_RSI_buy = False
+T_RSI_sell = False
+T_BB_buy = False
+T_BB_sell = False
+T_IPoC = False
+
+# API init.
+api = krakenex.API()  # key=config.API_KEY, secret=config.PRIVATE_KEY
+k = KrakenAPI(api)
+
+# Paper-trading, testing purposes.
+paper_trading = 10000  # 10k init.
+IPoC = 0
 
 
 # Functions
@@ -28,7 +49,7 @@ def get_coin_data(pair='ETHEUR'):
     :return:
     """
     global price_high, price_low, price_close
-    coin_data = k.get_ticker_information(pair='ETHEUR')
+    coin_data = k.get_ticker_information(pair=pair)
     price_high = round(float(coin_data['h'][0][0]), 2)
     price_low = round(float(coin_data['l'][0][0]), 2)
     price_close = round(float(coin_data['c'][0][0]), 2)
@@ -56,25 +77,49 @@ def save_coin_data():
 
 def calculate_rsi(period=14):
     """
-    Function to calculate RSI based on the data contained in 'coin_data.csv'.
-    :param period: int: number of collected values to calculate RSI over. none=14 (standard)
+    Function to calculate RSI based on the data within 'coin_data.csv'.
+    :param period: int: number of collected values to calculate over. none=14 (standard)
     :return:
     """
-    global last_RSI
-    df_price_close = pd.read_csv('coin_data.csv', usecols=['price_close'])[:period]
+    global last_RSI, RSI_ready
+    df_price_close = pd.read_csv('coin_data.csv', usecols=['price_close'])[-period:]
     if len(df_price_close) == period:
-        delta = df_price_close.diff(1)
+        RSI_ready = True
 
+        delta = df_price_close.diff(1)
         up = delta.clip(lower=0)
         down = -1 * delta.clip(upper=0)
         ema_up = up.ewm(com=13, adjust=False).mean()
         ema_down = down.ewm(com=13, adjust=False).mean()
+
         raw_rsi = 100 - (100 / (1 + (ema_up / ema_down)))
-        rsi = round(raw_rsi['price_close'][13], 2)
-        last_RSI = rsi
+        rsi = raw_rsi['price_close'][-1:]
+        last_RSI = round(rsi.values[0], 2)
 
     else:
-        print(f'[RSI] Waiting on data..  ({len(df_price_close)} of {period} data sets collected.)')
+        print(f'[RSI] Collecting data..  ({len(df_price_close)} of {period} data sets collected.)')
+
+
+def calculate_bollinger(period=20):
+    """
+    Function to calculate 'Bollinger bands' based on the data within 'coin_data.csv'.
+    :param period: int: number of collected values to calculate over. none=20 (standard)
+    :return:
+    """
+    global bollinger_upper, bollinger_lower, BB_ready
+    df_price_close = pd.read_csv('coin_data.csv', usecols=['price_close'])[-period:]
+
+    if len(df_price_close) == period:
+        BB_ready = True
+        raw_sma = df_price_close.rolling(period).mean()  # sma = 'simple moving average'.
+        sma = round(raw_sma['price_close'][-1:].values[0], 2)
+        raw_std = df_price_close.rolling(period).std()  # std = 'Standard deviation'.
+        std = round(raw_std['price_close'][-1:].values[0], 2)
+        bollinger_upper = sma + std * 2
+        bollinger_lower = sma - std * 2
+
+    else:
+        print(f'[Bollinger-bands] Collecting data..  ({len(df_price_close)} of {period} data sets collected.)')
 
 
 def check_triggers():
@@ -82,36 +127,138 @@ def check_triggers():
     Function to check if all BUY or SELL triggers have been met.
     :return:
     """
-    if last_RSI > RSI_sell:  # (Overbought trigger = sell)
-        print(f'[RSI] Overbought, Sell!')
-    elif last_RSI < RSI_buy:  # (Oversold trigger = buy)
-        print(f'[RSI] Oversold, Buy!')
-    else:
-        print(f'[RSI] {last_RSI}, hold..')
+    global T_RSI_buy, T_RSI_sell, T_BB_buy, T_BB_sell
+
+    # Checking each trigger.
+    if RSI_ready and BB_ready:
+        if last_RSI < RSI_buy:
+            T_RSI_buy = True
+        if price_close <= bollinger_lower:
+            T_BB_buy = True
+        if last_RSI > RSI_sell:
+            T_RSI_sell = True
+        if price_close >= bollinger_upper:
+            T_BB_sell = True
+
+    # All buy-related outcomes.
+    if T_RSI_buy and T_BB_buy:
+        print(f'[RSI] Oversold, Buy!\n'
+              f'[Bollinger-bands] Breakout, Buy!')
+        # kraken_buy():
+        paper_buy()
+    elif T_RSI_buy and not T_BB_buy:
+        print(f'[RSI] Oversold, Buy!\n'
+              f'[Bollinger-bands] no breakout though..\n'
+              f'[difference to lower band: {round((price_close - bollinger_lower), 2)}]')
+    elif T_BB_buy and not T_RSI_buy:
+        print(f'[Bollinger-bands] Breakout, Buy!\n'
+              f'[RSI] not yet..\n'
+              f'[Current RSI: {last_RSI}]')
+
+    # All sell-related outcomes.
+    if T_RSI_sell and T_BB_sell:
+        print(f'[RSI] Overbought, Sell!\n'
+              f'[Bollinger-bands] Breakout, Sell!')
+        # kraken_sell():
+        paper_sell()
+    elif T_RSI_sell and not T_BB_sell:
+        print(f'[RSI] Overbought, Sell!\n'
+              f'[Bollinger-bands] no breakout though..\n'
+              f'[difference to upper band: {round((bollinger_upper - price_close), 2)}]')
+    elif T_BB_sell and not T_RSI_sell:
+        print(f'[Bollinger-bands] Breakout, Sell!\n'
+              f'[RSI] not yet..\n'
+              f'[Current RSI: {last_RSI}]')
 
 
-def kraken_buy():
-    pass  # buy code here.
+def reset_triggers():
+    global T_RSI_buy, T_RSI_sell, T_BB_buy, T_BB_sell
+    T_RSI_buy = False
+    T_RSI_sell = False
+    T_BB_buy = False
+    T_BB_sell = False
+
+
+def kraken_balance():  # WIP
+    global account_balance
+    account_balance = k.get_account_balance()
+    print(account_balance)
+
+
+def kraken_buy():  # WIP
+    volume = floor(paper_trading/price_close)
+    api_nonce = str(int(time.time() * 1000))
+    api.query_private('AddOrder',
+                    {'nonce': api_nonce,
+                     'pair': TICKER,
+                     'type': 'buy',
+                     'ordertype': 'limit',
+                     'leverage': 'none',
+                     'volume': volume,
+                     'price': (price_close + BPD)
+                     })
 
 
 def kraken_sell():
     pass  # sell code here.
 
 
-def print_coin_data():  # test function.
-    print(f'[A new data set has been collected.]\n'
-          f'[High] €{price_high}\n'
-          f'[Low] €{price_low}\n'
-          f'[Close] €{price_close}\n'
-          f'')
+# For testing
+def paper_buy():
+    global paper_trading, IPoC, price_close, T_IPoC
+    if T_IPoC:
+        print('[BUY:prevented][Already in possession of crypto.]')
+    else:
+        IPoC = floor(paper_trading/price_close)
+        paper_trading -= IPoC * price_close
+        T_IPoC = True
+        print(f'[BUY:successful][We now own {IPoC} {TICKER[:2]}.]')
+
+
+# For testing
+def paper_sell():
+    global paper_trading, IPoC, price_close, T_IPoC
+    if not T_IPoC:
+        print('[SELL:prevented][No crypto in possession.]')
+    else:
+        paper_trading += IPoC * price_close
+        IPoC = 0
+        T_IPoC = False
+        print(f'[SELL:successful][current account worth: {paper_trading}.]')
+
+
+def status_rapport():
+    """
+    Function to print the latest data to console.
+    :return:
+    """
+    global last_RSI, bollinger_lower, bollinger_upper, price_close
+    if RSI_ready and BB_ready:
+        print(f'[STATUS RAPPORT]\n'
+              f'[RSI: {last_RSI}]\n'
+              f'-----------------\n'
+              f'[BOLLINGER]\n'
+              f'[high] {round(bollinger_upper, 2)}\n'
+              f'[price] {price_close}\n'
+              f'[low] {round(bollinger_lower, 2)}')
+    else:
+        print('[STATUS RAPPORT] Collecting data..')
 
 
 # Main loop.
 if __name__ == "__main__":
+    print(f'[KrakenBot:ACTIVE]\n'
+          f'[SETTINGS: [TICKER:{TICKER}][RSI]buy:{RSI_buy}/sell:{RSI_sell}]\n'
+          f'')
     while running:
-        get_coin_data()
-        # print_coin_data()
+        get_coin_data(pair=TICKER)
         save_coin_data()
-        calculate_rsi()
+        calculate_rsi(period=14)
+        calculate_bollinger(period=20)
         check_triggers()
-        time.sleep(interval)
+
+        status_rapport()
+        reset_triggers()
+
+        time.sleep(C_interval)
+        print()
